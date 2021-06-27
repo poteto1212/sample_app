4e2906891bb0a3a5d9a3f908791cfb61919c893e
Your Name <you@example.com>
2021-06-27 06:22:39 +0000

    init

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+#テストコマンド実行時に色を付ける
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
