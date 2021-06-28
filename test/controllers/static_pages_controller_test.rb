08d131026c8855ecb5770c170c3d9774f5b6de3b
Your Name <you@example.com>
2021-06-28 23:40:10 +0000

    init

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -33,7 +33,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
       get static_pages_contact_url
       assert_response:success
-      
+      assert_select "title","About | #{@base_title}"
   end    
   
 end
