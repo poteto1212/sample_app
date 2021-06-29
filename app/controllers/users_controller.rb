8aae3c76d5723b2a607e3b24abd5f90e79b6ee09
Your Name <you@example.com>
2021-06-29 11:48:09 +0000

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
-  def new
-  end
+    
+    def show
+        #paramsは動的urlパラメーターを取得するメソッド
+        @user=User.find(params[:id])
+    end
+    def new
+        @user=User.new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  #get 'users/new'
   get '/home',to:'static_pages#home'
   get '/help',to:'static_pages#help'
   get '/about',to:'static_pages#about'
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   get '/signup',to:'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  
+  #ユーザーリソース
+  #user/idのルーティングを設計
+  #resourcesを使う事で必要なリソースを全て
+  resources :users
 end
